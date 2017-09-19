5f7ad5e2d7962ee67d6479a5cb744ab6138ceeec
Hannes <Hannes@OJ>
2017-09-19 20:07:50 +0200

    Add project files.

---
+++ HTG_Parallel/frmParallel.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace HTG_Parallel_PI
+{
+    partial class frmParallel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(0, 0);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Start";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(284, 232);
+            this.panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ HTG_Parallel/frmParallel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HTG_Parallel_PI
+{
+    public partial class frmParallel : Form
+    {
+        const int Steps = 2000;
+        static ListBox lb = new ListBox();
+
+        public frmParallel()
+        {
+            InitializeComponent();
+        }
+
+        static void Duration<T>(Func<T> Calc)
+        {
+            var swPi = Stopwatch.StartNew();
+
+            var Result = Calc();
+
+            lb.Items.Add("Duration: " + swPi.Elapsed + " - Value: " + Result);
+
+        }
+
+        static double ParallelLinq()
+        {
+            double dblIncrement = 1.0 / (double)Steps;
+
+            return (from pe in ParallelEnumerable.Range(0, Steps)
+
+                    let objX = (pe + 0.5) * dblIncrement
+
+                    select 4.0 / (1.0 + objX * objX)).Sum() * dblIncrement;
+
+        }
+
+        static double ParallelForPi()
+        {
+            double dblSum = 0.0;
+
+            double dblIncrement = 1.0 / (double)Steps;
+
+            object objPI = new object();
+
+            Parallel.For(0, Steps, () => 0.0, (i, LoopState, objLocal) =>
+            {
+                double dblX = (i + 0.5) * dblIncrement;
+
+                return objLocal + 4.0 / (1.0 + dblX * dblX);
+
+            }, objTemp => { lock (objPI) dblSum += objTemp; });
+
+            return dblIncrement * dblSum;
+
+        }
+
+        static double ParallelForEachRangePartitioner()
+        {
+            double sum = 0.0;
+
+            double increment = 1.0 / (double)Steps;
+
+            object pi = new object();
+
+            Parallel.ForEach(Partitioner.Create(0, Steps), () => 0.0, (tplRange, LoopState, objLocal) =>
+            {
+                for (int i = tplRange.Item1; i < tplRange.Item2; i++)
+
+                {
+                    double x = (i + 0.5) * increment;
+
+                    objLocal += 4.0 / (1.0 + x * x);
+
+                }
+
+                return objLocal;
+
+            }, local => { lock (pi) sum += local; });
+
+            return increment * sum;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            lb.Width = panel1.Width - 10;
+            lb.Height = panel1.Height - 10;
+
+            lb.Left = 5;
+            lb.Top = 5;
+
+            lb.Visible = true;
+
+            this.panel1.Controls.Add(lb);
+
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            
+                Duration(() => ParallelLinq());
+                Duration(() => ParallelForPi());
+                Duration(() => ParallelForEachRangePartitioner());
+
+        }
+    }
+}
